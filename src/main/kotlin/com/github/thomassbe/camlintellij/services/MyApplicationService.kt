5c98e831f004282714322960bd272dad1d67a734
GitHub Action <action@github.com>
2021-03-30 11:08:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/thomassbe/camlintellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/thomassbe/camlintellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.thomassbe.camlintellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.thomassbe.camlintellij.MyBundle
 
 class MyApplicationService {
 
